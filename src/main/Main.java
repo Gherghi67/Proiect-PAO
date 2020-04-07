a99f3736de73c2a43cae716890045b101f110907
Gherghi67 <gherghinabogdan7@gmail.com>
2020-04-07 04:26:54 +0300

    Final code (part 1)

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,26 +2,52 @@ package main;
 
 import auth.AuthService;
 import card.Card;
+import credit.CreditService;
 import user.Client;
-import user.User;
+
 
 public class Main {
 
     public static void main(String[] args) {
-        AuthService service = AuthService.getInstance();
-        User user1 = new Client("ghergi", "12345", "123", 20, new Card());
+        AuthService authService = AuthService.getInstance();
 
-        service.registerNewUser(user1);
-        service.signOutUser();
+        CreditService creditService = CreditService.getInstance();
 
-        User user2 = new Client("ion", "421", "421", 30, new Card());
-        service.registerNewUser(user2);
-        service.signOutUser();
+        // register user
+        Client client = new Client("gherghi", "1990717280019", "12345", 20,
+                new Card("1234", "567", 200000));
+        client.setMarried(false);
+        client.setRetired(false);
+        authService.registerNewUser(client);
 
-        service.signInUser("12345", "123");
-        service.signOutUser();
+        // ask for help
+        creditService.askForHelp();
 
-        service.signInUser("02", "23");
+        // ask for interest rate
+        creditService.askForInterestRate((Client) authService.getCurrentUser());
 
+        // ask for a credit
+        creditService.askForACredit(client, 200000, 12);
+
+        // sign out user
+        authService.signOutUser();
+
+        // sign in user
+        authService.signInUser("1990717280019", "12345");
+
+        // ask details about your credit
+        creditService.askDetailsAboutYourCredit((Client) authService.getCurrentUser());
+
+        // pay installment
+        creditService.payInstallment((Client) authService.getCurrentUser(), 2000);
+
+        // check bank balance
+        System.out.println("Bank balance: " + authService.getCurrentUser().getCard().getBalance());
+
+        // ask for a delay
+        creditService.askForDelay(1);
+
+        // sign out user
+        authService.signOutUser();
     }
 }
